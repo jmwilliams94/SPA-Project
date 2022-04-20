cacc11fa4811f7cddea861cdac49aee927c35660
Jordan <jmwilliams94@icloud.com>
2022-04-21 09:05:00 +1000

    Started eventListener

--- Index.js
+++ Index.js
@@ -1,8 +1,21 @@
 
 //fetches API (working)
-async function fetchAPI() {
-    let response = await fetch('https://covid-19.dataflowkit.com/v1/');
-    let data = await response.text();
-    console.log(data);
+// let country = Data which is entered into search bar - then add this as string interpolation to the fetch?
+
+async function fetchAPI(location) {
+    fetch(`https://covid-19.dataflowkit.com/v1/${location}`).then(response => {
+        return response.json();   
+    })
+    .then(data => {
+        console.log(data)
+    })
 }
+
 fetchAPI();
+
+let customSearch = document.querySelector("searchbar");
+
+addEventListener("click", e => {
+    console.log(e);
+})
+
